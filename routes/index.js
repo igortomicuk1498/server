2ada824fad38022289c8ed2731074d852264f094
igortomicuk1498 <igortomichuk@gmail.com>
2020-02-17 13:00:56 -0800

    update

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render("index", {
-    title: "Мои контакты",
-    emailsVisible: true,
-    emails: ["gavgav@mycorp.com", "mioaw@mycorp.com"],
-    phone: "+1234567890"
-  });
+  res.render("index");
 
 });
 
